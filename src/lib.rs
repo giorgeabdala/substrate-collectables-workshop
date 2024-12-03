a2139a103db94f17a3e05c5da1b292fe02ec5af9
Giorge Abdala <giorgeabdala@gmail.com>
2024-12-03 16:28:40 -0300

    Then add a new field to the Kitty struct called price, which is an Option<BalanceOf<T>>.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@
 mod impls;
 
 use frame::prelude::*;
+use frame::traits::fungible::Inspect;
+use frame::traits::fungible::Mutate;
 pub use pallet::*;
 
 #[frame::pallet(dev_mode)]
@@ -15,13 +17,18 @@ pub mod pallet {
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+		type NativeBalance: Inspect<Self::AccountId> + Mutate<Self::AccountId>;
 	}
+
+	pub type BalanceOf<T> =
+		<<T as Config>::NativeBalance as Inspect<<T as frame_system::Config>::AccountId>>::Balance;
 
 	#[derive(Encode, Decode, TypeInfo, MaxEncodedLen)]
 	#[scale_info(skip_type_params(T))]
 	pub struct Kitty<T: Config> {
 		pub dna: [u8; 32],
 		pub owner: T::AccountId,
+		pub price: Option<BalanceOf<T>>,
 	}
 
 	#[pallet::storage]
@@ -49,6 +56,9 @@ pub mod pallet {
 		TooManyKitties,
 		DuplicateKitty,
 		TooManyOwned,
+		NoKitty,
+		NotOwner,
+		TransferToSelf,
 	}
 
 	#[pallet::call]
