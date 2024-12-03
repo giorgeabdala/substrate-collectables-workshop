eb196f039ed28c06d688462412d1a09b73168d22
Giorge Abdala <giorgeabdala@gmail.com>
2024-12-03 13:14:05 -0300

    add Storagevalue CountForKiitties

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 mod impls;
-mod tests;
 
 use frame::prelude::*;
 pub use pallet::*;
@@ -17,6 +16,9 @@ pub mod pallet {
 	pub trait Config: frame_system::Config {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 	}
+
+	#[pallet::storage]
+	pub(super) type CountForKitties<T: Config> = StorageValue<Value = u32>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
